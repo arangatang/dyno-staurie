00429734b80e2432b75ec156fa99a50fe52f9bba
Leonardo Araneda Freccero <leonardofreccero@gmail.com>
2022-07-16 15:51:44 +0200

    Fix edit story graph detection of ending and start nodes.

--- lambdas/update_story_lambda/update_story_lambda.py
+++ lambdas/update_story_lambda/update_story_lambda.py
@@ -4,6 +4,7 @@ from boto3.dynamodb.types import TypeDeserializer
 
 ddbclient = boto3.client("dynamodb")
 
+
 class ChapterOptions:
     options: List[dict]
 
@@ -18,9 +19,6 @@ class ChapterOptions:
 
     def get_path_for_choice(self, option: int) -> str:
         return str(self.options[option].get("next"))
-    
-    def has_options(self) -> bool:
-        return len(self.options) > 0
 
 
 class Chapter:
@@ -37,14 +35,13 @@ class Chapter:
             raise ValueError("Found chapter without text", chapter)
         else:
             self.text = chapter["text"]
-            
+
         self.chapter_id = chapter["chapter"]
         self.image = chapter["image"]
         self.options = ChapterOptions(chapter.get("options", []))
-
-        self.is_final_chapter = not self.options.has_options()
-        self.is_work_in_progress = chapter.get("is_work_in_progress", False)
-        self.is_start_node = chapter.get("is_start_node", False)
+        self.is_final_chapter = (
+            self.chapter_id.startswith(LAST_CHAPTER_PREFIX) or not self.options
+        )
 
     def get_choices(self) -> Dict[str, str]:
         if self.is_final_chapter:
@@ -55,58 +52,32 @@ class Chapter:
         if self.is_final_chapter:
             raise ValueError(f"No options available for chapter {self.chapter_id}")
         return self.options.get_path_for_choice(choice)
-    
-    def is_final(self):
-        return self.is_final_chapter
-    
-    def is_start(self):
-        return self.is_start_node
-    
-    def is_unfinished(self):
-        return self.is_work_in_progress
-
 
 
 def lambda_handler(event, context):
-    chapter_id = event.get("queryStringParameters", {}).get("chapter", None)
-    story_id = event.get("queryStringParameters", {}).get("story", None)
-    # chapter_id = "1"
-    # story_id = "test_story"
-    print(chapter_id, story_id)
-    if not chapter_id or not story_id:
-        return error_page()
-        # chapter_id = "3"
-        # story_id = "test_story"
+    params = event.get("queryStringParameters", {})
+    chapter_id = params.get("chapter_id", None)
+    new_option_id = params.get("new_option_id", None)
+    new_option_text = params.get("new_option_text", None)
+    story_id = params.get("story_id", None)
+    options = {
+        k.replace("option_", ""): v
+        for k, v in params.items()
+        if k.startswith("option_")
+    }
 
-    print(f"Querying for story: {story_id}")
-    response = ddbclient.query(
-        TableName=DDB_TABLE_NAME,
-        KeyConditionExpression="story = :story",
-        ExpressionAttributeValues={":story": {"S": story_id}},
-    )
-    # response =  {'Items': [{'chapter': {'S': '1'}, 'options': {'L': [{'M': {'next': {'S': '2'}}}]}, 'text': {'S': 'Once upon a time there was a young boy. '}, 'story': {'S': 'test_story'}, 'image': {'S': 'https://story-images.s3.eu-central-1.amazonaws.com/stories/test_story_1/images/1-2.png'}}, {'chapter': {'S': '2'}, 'options': {'L': [{'M': {'next': {'S': '3'}}}]}, 'text': {'S': 'The boy lived in a small house in a large forest'}, 'story': {'S': 'test_story'}, 'image': {'S': 'https://story-images.s3.eu-central-1.amazonaws.com/stories/test_story_1/images/2-7.png'}}, {'chapter': {'S': '3'}, 'options': {'L': [{'M': {'next': {'S': '3.1'}, 'text': {'S': 'The boy went outside to investigate'}}}, {'M': {'next': {'S': '3.2'}, 'text': {'S': 'The boy, who was used to these kind of sounds, quickly fell back asleep'}}}]}, 'text': {'S': 'One night, the boy woke up from a loud cracking sound just outside his house.'}, 'story': {'S': 'test_story'}, 'image': {'S': 'https://story-images.s3.eu-central-1.amazonaws.com/stories/test_story_1/images/3-3.png'}}, {'chapter': {'S': '3.1'}, 'options': {'L': [{'M': {'next': {'S': '-1'}, 'text': {'S': 'he fetched a flashlight inside, and then returned'}}}, {'M': {'next': {'S': '3.2'}, 'text': {'S': 'he went back to bed to wait for the morning light'}}}]}, 'text': {'S': 'The night was dark so the boy could not see anything.'}, 'story': {'S': 'test_story'}, 'image': {'S': 'https://story-images.s3.eu-central-1.amazonaws.com/stories/test_story_1/images/3.1-2.png'}}, {'chapter': {'S': '3.2'}, 'options': {'L': [{'M': {'next': {'S': '-1'}, 'text': {'S': 'The boy had some breakfast and then went outside to check what had made the sound'}}}]}, 'text': {'S': 'A few hours later the boy woke up to a bright morning.'}, 'story': {'S': 'test_story'}, 'image': {'S': 'https://story-images.s3.eu-central-1.amazonaws.com/stories/test_story_1/images/3.2-9.png'}}, {'chapter': {'S': '-1'}, 'text': {'S': 'The boy saw that the old oak tree had fallen over and inside sat a small pixie with purple eyes. Then the boy woke up.'}, 'story': {'S': 'test_story'}, 'image': {'S': 'https://story-images.s3.eu-central-1.amazonaws.com/stories/test_story_1/images/FINAL_CHAPTER-2.png'}}], 'Count': 6, 'ScannedCount': 6, 'ResponseMetadata': {'RequestId': '81RT0S7OISJUMUPABFMPQ61LKRVV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Tue, 12 Jul 2022 00:21:03 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '2128', 'connection': 'keep-alive', 'x-amzn-requestid': '81RT0S7OISJUMUPABFMPQ61LKRVV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '980735285'}, 'RetryAttempts': 0}}
-
-    print("Got response")
-    print(response)
-
-    items = response.get("Items", [])
-    if len(items) == 0:
-        return error_page()
-
-    chapters = {}
-
-    for item in items:
-        chapter = Chapter(chapter=from_dynamodb_to_json(item))
-        chapters[chapter.chapter_id] = chapter
-
-    nodes = get_nodes(chapters)
-    edges = get_edges(chapters)
-
-
+    # TODO
+    # 1. parse the passed data for obvious maliciousness
+    # 2. for each field, update the table if different
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "text/html"},
-        "body": generate_body(get_edit_windows(chapters, story_id=story_id, current_chapter=chapter_id), nodes, edges, chapter_id=chapter_id),
+        "body": generate_body(
+            get_edit_windows(chapters, story_id=story_id, current_chapter=chapter_id),
+            nodes,
+            edges,
+            chapter_id=chapter_id,
+        ),
     }
 
 
@@ -126,27 +97,23 @@ def get_edge(_from, to):
     )
 
 
-def get_node(id: str, is_work_in_progress: bool, is_final: bool, is_start: bool):   
-    if is_final:
-        node_color = "#FF0000" # red
-    elif is_start:
-        node_color = "#00FF00" # green
-    elif is_work_in_progress:
-        node_color = "#FFFF00" # yellow
-    else:
-        node_color = "#4DA4EA" # blue
-        
-    
-    color = 'color: { background: "%s"}' % node_color
-    return '{ id: "%s", label: "%s", %s },' % (id, id, color)
+def get_node(id: str, label: str):
+    green = "#00FF00"
+    red = "#FF0000"
+    color = ""
+    node_color = "#4DA4EA"
+    if id.startswith("-"):
+        node_color = red
+    elif id.startswith("1"):
+        node_color = green
+
+    if node_color:
+        color = 'color: { background: "%s"}' % node_color
+    return '{ id: "%s", label: "%s", %s },' % (id, label, color)
 
 
 def get_nodes(chapters: Dict[str, Chapter]):
-    nodes = []
-    for key, chapter in chapters.items():
-        nodes.append(get_node(key, chapter.is_unfinished(), chapter.is_final(), chapter.is_start()))
-        
-    return "\n".join(nodes)
+    return "\n".join([get_node(key, key) for key in chapters.keys()])
 
 
 def get_edit_window_existing_option(option_id: str, option_text: str):
@@ -158,26 +125,40 @@ def get_edit_window_existing_option(option_id: str, option_text: str):
     )
 
 
-def get_edit_window(chapter: Chapter, next_choice_id: int, story_id: str, should_show: bool):
+def get_edit_window(
+    chapter: Chapter, next_choice_id: int, story_id: str, should_show: bool
+):
     options = []
     for option_id, option_text in chapter.get_choices().items():
         options.append(get_edit_window_existing_option(option_id, option_text))
-        
+
     return get_edit_window_html().format(
         CHAPTER_ID=chapter.chapter_id,
-        IMAGE= chapter.image, # TODO remove
+        IMAGE=chapter.image,
         EXISTING_OPTIONS="""
-        """.join(options),
+        """.join(
+            options
+        ),
         STORY_ID=story_id,
         NEXT_CHOICE_ID=next_choice_id,
         CHAPTER_TEXT=chapter.text,
-        DISPLAY=' edit-window-clicked" style="display: block' if should_show else ''
+        DISPLAY=' edit-window-clicked" style="display: block' if should_show else "",
     )
 
 
 def get_edit_windows(chapters: Dict[str, Chapter], story_id: str, current_chapter: str):
     next_choice_id = max([int(id.split(".")[0]) for id in chapters]) + 1
-    return "".join([get_edit_window(chapter, next_choice_id, story_id=story_id, should_show= current_chapter == chapter_id) for chapter_id, chapter in chapters.items()])
+    return "".join(
+        [
+            get_edit_window(
+                chapter,
+                next_choice_id,
+                story_id=story_id,
+                should_show=current_chapter == chapter_id,
+            )
+            for chapter_id, chapter in chapters.items()
+        ]
+    )
 
 
 def get_edges(chapters: Dict[str, Chapter]):
@@ -186,21 +167,19 @@ def get_edges(chapters: Dict[str, Chapter]):
         for choice in chapter.get_choices().keys():
             edges.append(get_edge(chapter_id, choice))
     return """
-    """.join(edges)
+    """.join(
+        edges
+    )
+
 
 def select_node(node_id):
     return node_id if node_id else ""
 
+
 def generate_body(edit_windows, nodes, edges, chapter_id):
     html = get_html()
     style = get_style()
-    formatted = html % (
-        style, 
-        edit_windows, 
-        nodes, 
-        edges,
-        select_node(chapter_id)
-    )
+    formatted = html % (style, edit_windows, nodes, edges, select_node(chapter_id))
     # print(formatted)
     return formatted
 
@@ -210,6 +189,7 @@ DDB_TABLE = "arn:aws:dynamodb:eu-central-1:679585051464:table/stories"
 DDB_TABLE_NAME = "stories"
 
 LAST_CHAPTER_PREFIX = "-"
+
 
 def get_style():
     return """
@@ -289,6 +269,7 @@ def get_style():
     }
     """
 
+
 def get_html():
     return """
     <!DOCTYPE html>
@@ -353,8 +334,9 @@ def get_html():
     </html>
     """
 
+
 def get_edit_window_html():
-    return  """
+    return """
     <div class="edit-window{DISPLAY}" id="edit-window-{CHAPTER_ID}">
         <img src="{IMAGE}" loading="lazy">
         <form action="/process_edit_story" method="get">
@@ -363,6 +345,7 @@ def get_edit_window_html():
             {EXISTING_OPTIONS}
             <label for="new_option">New choice text: </label>
             <input type="text" id="new_option_text" name="new_option_text"><br><br>
+            
             <input type="hidden" id="new_option_id" name="new_option_id" value="{NEXT_CHOICE_ID}"><br><br>
             <input type="hidden" name="story_id" value={STORY_ID} class="hidden">
             <input type="hidden" name="chapter_id" value={CHAPTER_ID} class="hidden">
@@ -372,5 +355,119 @@ def get_edit_window_html():
     </div>
     """
 
-if __name__=="__main__":
-    lambda_handler(None, None)
+
+if __name__ == "__main__":
+    lambda_handler(
+        {
+            "version": "1.0",
+            "resource": "/update_story",
+            "path": "/default/update_story",
+            "httpMethod": "GET",
+            "headers": {
+                "Content-Length": "0",
+                "Host": "bne1jvubt0.execute-api.eu-central-1.amazonaws.com",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+                "X-Amzn-Trace-Id": "Root=1-62cdf9ca-6555c8a93bb1c77d34dbff10",
+                "X-Forwarded-For": "109.40.242.10",
+                "X-Forwarded-Port": "443",
+                "X-Forwarded-Proto": "https",
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "en-US,en;q=0.9,sv-SE;q=0.8,sv;q=0.7",
+                "dnt": "1",
+                "sec-ch-ua": '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": '"Windows"',
+                "sec-fetch-dest": "document",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "none",
+                "sec-fetch-user": "?1",
+                "upgrade-insecure-requests": "1",
+            },
+            "multiValueHeaders": {
+                "Content-Length": ["0"],
+                "Host": ["bne1jvubt0.execute-api.eu-central-1.amazonaws.com"],
+                "User-Agent": [
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+                ],
+                "X-Amzn-Trace-Id": ["Root=1-62cdf9ca-6555c8a93bb1c77d34dbff10"],
+                "X-Forwarded-For": ["109.40.242.10"],
+                "X-Forwarded-Port": ["443"],
+                "X-Forwarded-Proto": ["https"],
+                "accept": [
+                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+                ],
+                "accept-encoding": ["gzip, deflate, br"],
+                "accept-language": ["en-US,en;q=0.9,sv-SE;q=0.8,sv;q=0.7"],
+                "dnt": ["1"],
+                "sec-ch-ua": [
+                    '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"'
+                ],
+                "sec-ch-ua-mobile": ["?0"],
+                "sec-ch-ua-platform": ['"Windows"'],
+                "sec-fetch-dest": ["document"],
+                "sec-fetch-mode": ["navigate"],
+                "sec-fetch-site": ["none"],
+                "sec-fetch-user": ["?1"],
+                "upgrade-insecure-requests": ["1"],
+            },
+            "queryStringParameters": {
+                "_text": "One night, the boy woke up from a loud cracking sound just outside his house.",
+                "chapter_id": "3",
+                "new_option_id": "4",
+                "new_option_text": "",
+                "option_3.1": "The boy went outside to investigate",
+                "option_3.2": "The boy, who was used to these kind of sounds, quickly fell back asleep",
+                "story_id": "test_story",
+            },
+            "multiValueQueryStringParameters": {
+                "_text": [
+                    "One night, the boy woke up from a loud cracking sound just outside his house."
+                ],
+                "chapter_id": ["3"],
+                "new_option_id": ["4"],
+                "new_option_text": [""],
+                "option_3.1": ["The boy went outside to investigate"],
+                "option_3.2": [
+                    "The boy, who was used to these kind of sounds, quickly fell back asleep"
+                ],
+                "story_id": ["test_story"],
+            },
+            "requestContext": {
+                "accountId": "679585051464",
+                "apiId": "bne1jvubt0",
+                "domainName": "bne1jvubt0.execute-api.eu-central-1.amazonaws.com",
+                "domainPrefix": "bne1jvubt0",
+                "extendedRequestId": "VLP3sgx6FiAEMHw=",
+                "httpMethod": "GET",
+                "identity": {
+                    "accessKey": None,
+                    "accountId": None,
+                    "caller": None,
+                    "cognitoAmr": None,
+                    "cognitoAuthenticationProvider": None,
+                    "cognitoAuthenticationType": None,
+                    "cognitoIdentityId": None,
+                    "cognitoIdentityPoolId": None,
+                    "principalOrgId": None,
+                    "sourceIp": "109.40.242.10",
+                    "user": None,
+                    "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+                    "userArn": None,
+                },
+                "path": "/default/update_story",
+                "protocol": "HTTP/1.1",
+                "requestId": "VLP3sgx6FiAEMHw=",
+                "requestTime": "12/Jul/2022:22:46:34 +0000",
+                "requestTimeEpoch": 1657665994634,
+                "resourceId": "ANY /update_story",
+                "resourcePath": "/update_story",
+                "stage": "default",
+            },
+            "pathParameters": None,
+            "stageVariables": None,
+            "body": None,
+            "isBase64Encoded": False,
+        },
+        None,
+    )
